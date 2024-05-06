ff3324e4451bf9fcee1e8b4679793a8d25ac574e
Camiboro <130386618+Camiboro@users.noreply.github.com>
2024-05-06 16:56:58 +0000

    firs commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,40 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    return (
+        <div className="App">
+            <nav className="navbar">
+                <button>Home</button>
+                <button>About</button>
+                <button>Service</button>
+                <button>Contact</button>
+            </nav>
+
+            <div className="container"> 
+                <div className="jumbotron">
+                    <h1 className="jumbotron-title">Título del Jumbotron</h1>
+                    <p className="jumbotron-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
+                    </p>
+                    <button className="jumbotron-button">click to action</button>
+                </div>  
+
+                <div className="cards-container">
+                    {Array(4).fill(0).map((_, index) => (
+                        <div key={index} className="card">
+                            <img src={"http://via.placeholder.com/500x325"} alt="Card" />
+                            <p>Texto descriptivo aquí</p>
+                            <button className="blue-button">Find out more</button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <footer className="footer">
+                <p>© 2023 Todos los derechos reservados.</p>
+            </footer>
+        </div>
+    );
 };
 
 export default Home;
